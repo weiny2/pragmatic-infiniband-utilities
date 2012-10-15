1e3d0ea193369566dad2cd007fe5f73a0033d69f
Ira Weiny <weiny2@llnl.gov>
2012-10-15 17:15:17 -0400

    add copyright to new scripts

--- scripts/dump-lfts.py
+++ scripts/dump-lfts.py
@@ -1,4 +1,30 @@
 #!/usr/bin/python
+#################################################################################
+#
+#  Copyright (C) 2012 Lawrence Livermore National Security
+#  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+#  Written by Ira Weiny weiny2@llnl.gov
+#  UCRL-CODE-235440
+#
+#  This file is part of pragmatic-infiniband-tools (PIU), useful tools to manage
+#  Infiniband Clusters.
+#  For details, see http://www.llnl.gov/linux/.
+#
+#  PIU is free software; you can redistribute it
+#  and/or modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the License,
+#  or (at your option) any later version.
+#
+#  PIU is distributed in the hope that it will be
+#  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#  Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  PIU; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+#
+#################################################################################
 
 from threading import Thread
 from threading import Lock
